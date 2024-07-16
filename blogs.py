ef5a30e062a9668803d8435924734008d448f1a6
austin <doublehoon2@gmail.com>
2024-07-16 18:41:41 +0900

    blogs

---
+++ blogs.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+import sys
+spark = (
+    SparkSession.builder
+        .master("local[*]")
+        .appName("blogs")
+        .getOrCreate()
+)
+
+if len(sys.argv) != 2:
+    print("path <> :", file= sys.stderr)
+    exit(-1)
+
+df = (spark.read.format('json').load(sys.argv[1]))
+
+df.show()
