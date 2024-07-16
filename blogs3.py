a382ebaf07754619c0e12018f676d427129972e3
austin <doublehoon2@gmail.com>
2024-07-16 23:56:49 +0900

    date parsing

--- blogs3.py
+++ blogs3.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from pyspark.sql.types import StringType, ArrayType, StructField, StructType, IntegerType, LongType
+from pyspark.sql.types import StringType, ArrayType, StructField, StructType, IntegerType, LongType, DateType
 
 
 import sys
@@ -25,19 +25,31 @@ blogs_schema = StructType([
     StructField("First", StringType(), nullable= False),
     StructField("Last", StringType(), nullable= False),
     StructField("Url", StringType(), nullable= False),
-    StructField("Published", StringType(), nullable= False),
+    StructField("Published", DateType(), nullable= False),
     StructField("Hits", IntegerType(), nullable= False),
     StructField("Campaigns", ArrayType(StringType()), nullable= True),
 ])
 
 df = (spark.read.format('json')
     #   .option('multiline', True)
-      .schema(blogs_schema)
+    #   .schema(blogs_schema)
       .load(sys.argv[1]))
 
-df2 = (
-    df.select("Id", "First", "Last", "Url", "Published", "Hits", explode(df.Campaigns).alias("Campaigns")).show(10) 
-    )
+df.printSchema()
+df.show(10)
+# ts_pattern ="MM/dd/yyyy"
+
+df2 = (df.select(to_date("Published", "M/d/yyyy").alias("date")).show())
+# df2.show(10)
+
+# df2.printSchema()
+# df2.show(10)
+# df3 = (
+#     df2.select("Id", "First", "Last", "Url",
+#             "Published",
+#             "Hits",
+#             explode(df.Campaigns).alias("Campaigns")).show(10) 
+#     )
 
 # df3.printSchema()
 # df3.show(10)
