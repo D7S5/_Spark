50b5e93499616b85869b6bcb9b762cdfb0286075
austin <doublehoon2@gmail.com>
2024-07-18 19:28:54 +0900

    flights_parquet

---
+++ flights_parquet.py
@@ -0,0 +1,22 @@
+import sys
+
+from pyspark.sql import SparkSession
+
+from config.mysql_conf import *
+
+spark = (
+    SparkSession.builder
+    .master('local[*]')
+    .appName('flights_parquet')
+    .config('spark.driver.extraClassPath', jars)
+    .getOrCreate()
+)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('path <> :' , file= sys.stderr)
+        sys.exit(-1)
+
+df = spark.read.parquet(sys.argv[1])
+
+df.show(10)
