be58ffe1a554db2b75eab2bddc613906aaa76453
austin <doublehoon2@gmail.com>
2024-07-20 15:10:25 +0900

    us-iot

--- iot_devices.py
+++ iot_devices.py
@@ -1,14 +1,14 @@
 import sys
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
 from config.mysql_conf import *
 from schema.iot_devices import iot_devices_schema
 spark = (
     SparkSession.builder
     .master('local[*]')
     .appName('lot_devices')
-    .config("spark.jars", jars)
-    .config("spark.sql.legacy.charVarcharAsString", True) # fix : Longtext 
+    .config('spark.jars', jars)
     .getOrCreate()
 )
 
@@ -19,21 +19,29 @@ if len(sys.argv) != 2:
 schema = iot_devices_schema
 
 df = (
-    spark.read.format('csv')
-        .option('header', True)
-        .schema(schema)
+    spark.read.format('json')
         .load(sys.argv[1])
 )
 # df.printSchema()
 # df.show(2)
 
-table_name = 'iot_devices'
+df2 = (
+    df.select('device_id','device_name', 'ip','cca2',
+               'cca3', 'cn', 'latitude', 'longitude',
+                'scale', 'temp','humidity', 'battery_level',
+                'c02_level', 'lcd', 'timestamp')
+    .filter(expr("""cca3 == 'USA'""") )
+)
+
+db_name = 'db4'
+table_name = 'US_iot_devices'
+
 mode = 'overwrite'
 
-(df.write
+(df2.write
     .format('jdbc')
-    .option("driver","com.mysql.cj.jdbc.Driver")
-    .option('url' , url_database1)
+    .option('driver','com.mysql.cj.jdbc.Driver')
+    .option('url' , url + db_name)
     .option('mode' , mode)
     .option('dbtable' , table_name)
     .option('user', 'austin')
