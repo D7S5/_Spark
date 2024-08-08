00528b9efbc25b9cb01f930d17ae6bf1e001e574
austin <doublehoon2@gmail.com>
2024-08-08 18:41:36 +0900

    hmm

--- read_csv.py
+++ read_csv.py
@@ -2,6 +2,7 @@ import sys
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+from pyspark.sql import Window
 from config.mysql_conf import *
 
 spark = (
@@ -28,17 +29,26 @@ df2 = (
     df.filter(expr("""DEST_COUNTRY_NAME == 'United States'"""))
         .groupBy('DEST_COUNTRY_NAME', 'ORIGIN_COUNTRY_NAME')
         .agg(sum('count').alias('2010-2015_count'))
-        .orderBy(desc("2010-2015_count"))
+        .orderBy(desc('2010-2015_count'))
         )
 
+rank_function = (rank()
+                 .over(Window.orderBy('2010-2015_count'))
+                 )
+rank = (df2.withColumn('2010-2015_count', rank_function))
+
+rank.show(10)
+
+
+# df2.show(10)
 dbname = 'db4'
 dbtable = 'flights' # table name long text  2010-2015_United_state flights count
 
-(df2.write.format('jdbc')
-    .option('url', url+dbname)
-    .option('mode', 'overwrite')
-    .option('driver', 'com.mysql.cj.jdbc.Driver')
-    .option('dbtable', dbtable)
-    .option('user', 'austin')
-    .option('password', password)
-    .save())
+# (df2.write.format('jdbc')
+#     .option('url', url+dbname)
+#     .option('mode', 'overwrite')
+#     .option('driver', 'com.mysql.cj.jdbc.Driver')
+#     .option('dbtable', dbtable)
+#     .option('user', 'austin')
+#     .option('password', password)
+#     .save())
