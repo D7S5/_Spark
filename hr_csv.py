c0c1019baa5a2f8f5923bf256e27decbbdfb29f1
austin <doublehoon2@gmail.com>
2024-08-22 17:41:46 +0900

    spark-doc

---
+++ hr_csv.py
@@ -0,0 +1,43 @@
+import sys
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from config.mysql_conf import *
+
+spark = (
+    SparkSession.builder
+    .master('local[*]')
+    .appName('flights_csv')
+    .config('spark.driver.extraClassPath', jars)
+    .getOrCreate()
+)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('path <> :' , file= sys.stderr)
+        sys.exit(-1)
+
+df = (
+    spark.read
+    .option('header', True)
+    .option("inferschema", True)
+    .csv(sys.argv[1])
+    )
+
+
+df2 = (df.select('*'))
+
+df2.show(20)
+
+
+dbname = 'db3'
+dbtable = 'HR_dataset' # table name long text  2010-2015_United_state flights count
+
+(df.write.format('jdbc')
+    .option('url', url+dbname)
+    .option('mode', 'overwrite')
+    .option('driver', 'com.mysql.cj.jdbc.Driver')
+    .option('dbtable', dbtable)
+    .option('user', 'austin')
+    .option('password', password)
+    .save())
