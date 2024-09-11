4ddd1e7446a4e82ffff0c2a55ddc1895db79319b
austin <doublehoon2@gmail.com>
2024-09-11 23:31:14 +0900

    room

--- room.py
+++ room.py
@@ -8,7 +8,7 @@ from config.mysql_conf import *
 spark = (
     SparkSession.builder
         .master("local[*]")
-        .appName("energy_consume_stage1")
+        .appName("room")
         .config("spark.jars" , jars)
         .getOrCreate()
 )
@@ -23,24 +23,20 @@ df = (spark.read
       .option('inferschema', True)
       .csv(sys.argv[1])
 )
-
-# df.show(10)
-# print(df.dtypes)
-ts_pattern = "dd MMM yyyy"
-ts_pattern2= 'HH:mm:ss'
+# "08-12-2018 06:57"
+ts_pattern = "MM-dd-yyyy HH:mm"
 
 df2 = (df.select(
-            to_date('TxnDate', ts_pattern).alias('TxnDate'),
-              'TxnTime', date_format('TxnTime', ts_pattern2).alias('Txn_time'),
-              'Consumption')
-              .drop('TxnTime')
-              .withColumnRenamed('Txn_Time','TxnTime')
-              .orderBy('TxnDate','TxnTime', ascending=[True, True]))
-
-# df3 = (df2.select('*')).show(10)
+    'id', 'room_id/id',
+    to_timestamp('noted_date', ts_pattern).alias('noted_date'),
+    'temp',
+    'out/in'
+))
+# df2.printSchema()
+# df2.show(20)
 
 db_name = "db3"
-table_name = "energy_consume_stage2"
+table_name = "room"
 
 (df2.write.format('jdbc')
     .option('url', url + db_name)
@@ -49,3 +45,5 @@ table_name = "energy_consume_stage2"
     .option('user', 'austin')
     .option('password', password)
     .save())
+
+
