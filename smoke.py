8b0195ad951d74bad63420fd3e01227a4f51f272
austin <doublehoon2@gmail.com>
2024-09-16 01:08:06 +0900

    avg

--- smoke.py
+++ smoke.py
@@ -27,19 +27,30 @@ df = (spark.read
 # df.printSchema()
 # df.show(20)
 
-df2 = (df.select('*')
-                 .withColumnRenamed('Fire Alarm', 'Fire_Alarm'))
+df2 = (df
+       .select('*')
+       .withColumnRenamed('Fire Alarm', 'Fire_Alarm')
+       .filter(col('Fire_Alarm') == True)
+       )
+        
+df3 = (df2
+       .select(
+        avg(col('Humidity[%]')).alias('AVG(Humidity[%])'),
+        avg(col('`PM1.0`')).alias('AVG(PM1.0)'),
+        avg(col('`PM2.5`')).alias('AVG(PM2.5)'),
+        avg(col('`NC0.5`')).alias('AVG(NC0.5)'),
+        avg(col('`NC2.5`')).alias('AVG(NC2.5)'))
+        ).show()
+# ?
 
-df2.show(20)
+# db_name = "db3"
+# table_name = "smoke_detection"
 
-db_name = "db3"
-table_name = "smoke_detection"
-
-(df2.write.format('jdbc')
-    .option('url', url + db_name)
-    .option('driver', 'com.mysql.cj.jdbc.Driver')
-    .option('dbtable', table_name)
-    .option('user', 'austin')
-    .option('password', password)
-    .save())
+# (df2.write.format('jdbc')
+#     .option('url', url + db_name)
+#     .option('driver', 'com.mysql.cj.jdbc.Driver')
+#     .option('dbtable', table_name)
+#     .option('user', 'austin')
+#     .option('password', password)
+#     .save())
 
