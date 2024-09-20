a634d196929a146073c58662189e921da32c1a95
austin <doublehoon2@gmail.com>
2024-09-21 01:45:16 +0900

    us_popluations.py

--- us_populations.py
+++ us_populations.py
@@ -13,13 +13,6 @@ if len(sys.argv) != 2:
     print("Usage : source file <file>", file=sys.stderr)
     sys.exit(-1)
 
-schema = StructType([
-    StructField('zipcode', StringType(), nullable=False),
-    StructField('city', StringType(), nullable=False),
-    StructField('loc', DoubleType(), nullable=False),
-    StructField('pop', IntegerType(), nullable=False),
-    StructField('state', StringType(), nullable=False)
-])
 df = (spark.read.format('json').load(sys.argv[1]))
 
-df.show(10)
+df.show(10, truncate=False)
